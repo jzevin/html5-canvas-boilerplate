8378b31c6fe89682ab37b9429d5eaf0d9b99513c
phillyDesignr <jeremy.zevin@gmail.com>
2017-01-09 13:21:03 -0500

    modified a touch just for aesthetics

--- src/app.js
+++ src/app.js
@@ -18,9 +18,15 @@ class App {
     window.requestAnimationFrame((t)=>{
       this.loop(t);
     });
-    for (var i = 0; i < 20; i++) {
-      this.ctx.clearRect(rnd.int(0,this.w),0,1,this.h);
+    for (var i = 0; i < 99; i++) {
+      this.ctx.globalAlpha = rnd.real(0.01,0.5);
+      this.ctx.fillStyle = 'black';
       this.ctx.fillRect(0,rnd.int(0,this.h),this.w,1);
+    }
+    for (var i = 0; i < 3; i++) {
+      this.ctx.globalAlpha = rnd.real(0.01,0.25);
+      this.ctx.fillStyle = 'white';
+      this.ctx.fillRect(rnd.int(0,this.w),0,rnd.real(0.0001,10.9),this.h);
     }
   }
 }
